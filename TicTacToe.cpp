6bbe7b889c456bfe5ea5e1619b286b1365f1d38a
kusi3680 <45445645+kusi3680@users.noreply.github.com>
2020-09-25 20:30:10 -0600

    person 1 added displayboard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -14,3 +14,15 @@ int * CreateBoard ();
     
     return arr;
 }
+
+void DisplayBoard(vector< vector<string> > board)
+{
+        for (int j = 0; j<3 ; j++)
+    {
+        for(int i = 0; i<3;i++)
+        {
+            cout<< board[j][i]<<" ";
+        }
+            cout<<endl;
+    }
+}
